911dfee6a4971721425621e2ff376a39f731c7f7
muzakidian <muzakidian1@gmail.com>
2023-05-17 09:46:21 +0700

    attack animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+
+public enum PlayerState{
+    walk,
+    attack,
+    interact
+}
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 15;
+    public PlayerState currentState;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
     private Vector2 inputMovement;
     public VectorValue startingPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -20,9 +31,38 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetButtonDown("attack") && currentState != PlayerState.attack )
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();    
+        }
+        
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+    }
+
+    void UpdateAnimationAndMove()
+    {
         if (change != Vector3.zero)
         {
             MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        } else
+        {
+            animator.SetBool("moving", false);
         }
     }
 
