d7c3344f59c34adeee7453243322894a775b69f4
muzakidian <muzakidian1@gmail.com>
2023-05-29 03:13:40 +0700

    nama tempat

---
+++ Assets/Scripts/NamaTempat.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NamaTempat : MonoBehaviour
+{
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            if(needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
+
+        }
+    }
+        private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
+    }
+}
