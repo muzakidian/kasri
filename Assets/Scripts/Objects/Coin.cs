7731d5f92d6eb396393f40ec51d9643c259f886f
muzakidian <muzakidian1@gmail.com>
2023-06-05 13:14:05 +0700

    Coin

---
+++ Assets/Scripts/Objects/Coin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : Powerup
+{
+    public Inventory playerInventory;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerInventory.coins += 1;
+            powerupSignal.Raise();
+            Destroy(this.gameObject);
+        }
+    }
+}
