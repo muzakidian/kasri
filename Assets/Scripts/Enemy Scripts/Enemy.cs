1f311068ed60018b979d8db9591cc6a5cd368f1e
muzakidian <muzakidian1@gmail.com>
2023-06-05 10:17:26 +0700

    enemy death effects

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -17,6 +17,7 @@ public class Enemy : MonoBehaviour {
     public string enemyName;
     public int baseAttack;
     public float moveSpeed;
+    public GameObject deathEffect;
 
     private void Awake()
     {
@@ -28,7 +29,17 @@ public class Enemy : MonoBehaviour {
         health -= damage;
         if(health <= 0)
         {
+            DeathEffect();
             this.gameObject.SetActive(false);
+        }
+    }
+
+        private void DeathEffect()
+    {
+        if(deathEffect != null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
         }
     }
 
