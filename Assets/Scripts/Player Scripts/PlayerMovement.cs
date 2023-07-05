8182658aff01e3ef3c0db64766000ca7ee5600fd
muzakidian <muzakidian1@gmail.com>
2023-07-05 18:48:07 +0700

    player kebal setelah hit

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -29,6 +29,14 @@ public class PlayerMovement : MonoBehaviour
     public GameObject peluru;
     public Item watergun;
 
+    [Header("Fungsi Kebal")]
+    public Color flashColor;
+    public Color regularColor;
+    public float flashDuration;
+    public int numberOfFlashes;
+    public Collider2D triggerCollider;
+    public SpriteRenderer mySprite;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -175,10 +183,26 @@ public class PlayerMovement : MonoBehaviour
     {
         if (myRigidbody != null)
         {
+            StartCoroutine(FlashCo());
             yield return new WaitForSeconds(knockTime);
             myRigidbody.velocity = Vector2.zero;
             currentState = PlayerState.idle;
             myRigidbody.velocity = Vector2.zero;
         }
+    }
+
+    private IEnumerator FlashCo()
+    {
+        int temp = 0;
+        triggerCollider.enabled = false;
+        while(temp < numberOfFlashes)
+        {
+            mySprite.color = flashColor;
+            yield return new WaitForSeconds(flashDuration);
+            mySprite.color = regularColor;
+            yield return new WaitForSeconds(flashDuration);
+            temp++;
+        }
+        triggerCollider.enabled = true;
     }
 }
